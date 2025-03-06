6dbbe7cc8dd647b996bc5f0814c64f05be920961
MASTER <dahshury@gmail.com>
2025-03-06 04:31:10 +0300

    fixing location message

--- app/services/assistant_functions.py
+++ app/services/assistant_functions.py
@@ -11,7 +11,7 @@ from hijri_converter import convert
 
 def send_business_location(wa_id, json_dump=False):
     try:
-        status = send_whatsapp_location(wa_id, config["BUSINESS_LATITUDE"], config["BUSINESS_LONGITUDE"])
+        status = send_whatsapp_location(wa_id, config["BUSINESS_LATITUDE"], config["BUSINESS_LONGITUDE"], config['BUSINESS_NAME'], config['BUSINESS_ADDRESS'])
         result = {"success": False, "message": "System error occurred. try again later."} if status.get("status") == "error" else {"success": True, "message": "Location sent."}
         return json.dumps(result) if json_dump else result
     except Exception as e:
